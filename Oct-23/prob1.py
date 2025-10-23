0656de9a78794c997dff1111b3cdeb4e117c52e2
Sampath Kumar <sam.varma2503@gmail.com>
2025-10-23 23:50:24 +0530

    added new file

---
+++ Oct-23/prob1.py
@@ -0,0 +1,16 @@
+#Print the second item in the fruits list.
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+#Change the value from "apple" to "kiwi", in the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = 'kiwi'
+print(fruits)
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append('sweet')
+print(fruits)
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(4,'hot')
+print(fruits)
---
+++ Oct-23/prob2.py
@@ -0,0 +1,5 @@
+fruits = ["apple", "banana", "cherry"]
+fruits.append('sweet')
+print(fruits)
+print(fruits[1])
+print(fruits.)

85e8eb455e75cb4c16f5cca9b21a09fccf53a1d7
sampathk-data <sampath.kodisherla03@gmail.com>
2025-11-02 18:41:07 +0530

    add new schema for BQ

--- Oct-26/prob3.py
+++ Oct-26/prob3.py
@@ -4,3 +4,19 @@ for i in range(1,11):
     sum = sum + i 
     print(sum)
 
+
+
+git config --global user.name "sampathk-data"
+
+git config --global user.email "sampath.kodisherla03@gmail.com"
+
+
+bq load --source_format=.csv --autodetect DATASET.TABLE PATH_TO_SOURCE [SCHEMA_DEFINITION]
+bq load --source_format=CSV --autodetect --skip_leading_rows=1 my_dataset.my_table data.csv
+
+
+bq load \
+    --source_format=CSV \
+    --autodetect \
+    dataset_1.cust_1 \
+    gs://new-b43-bucket-2/customer_details.csv
